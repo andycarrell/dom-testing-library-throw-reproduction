736868e5e31f554569ccbd269bbcadd14f2cea18
Andy Carrell <andycarrell@users.noreply.github.com>
2019-07-03 08:32:17 +1200

    Updates index.test.js with reproduction

--- src/__tests__/index.test.js
+++ src/__tests__/index.test.js
@@ -1,10 +1,30 @@
-import {render} from 'react-testing-library'
-import React from 'react'
+import React from "react";
+import { render, cleanup } from "react-testing-library";
+
 import 'jest-dom/extend-expect'
-import MyComponent from '../'
 
-it('renders greeting', () => {
-  const {container, queries} = render(<MyComponent />)
-  expect(container.firstChild).toHaveTextContent('Hello, World')
-})
+const ComponentWillThrow = ({ shouldThrow }) => {
+	if (shouldThrow) {
+		throw new Error("Component did throw after all");
+	}
 
+	return <p>I guess no throw this time</p>;
+};
+
+afterEach(cleanup);
+
+test("renders without erroring", () => {
+	const { getByText } = render(<ComponentWillThrow />);
+
+	expect(getByText("I guess no throw this time")).toBeInTheDocument();
+});
+
+test("expect this to throw", () => {
+	expect(() => render(<ComponentWillThrow shouldThrow />)).toThrow();
+});
+
+test("still render without erroring", () => {
+	const { getByText } = render(<ComponentWillThrow />);
+
+	expect(getByText("I guess no throw this time")).toBeInTheDocument();
+});
