928f14484023080ff20ee0a1158c9dcf1c455605
Andy Carrell <andycarrell@users.noreply.github.com>
2019-07-10 09:47:57 +1200

    Adds throw to console error

--- src/__tests__/index.test.js
+++ src/__tests__/index.test.js
@@ -1,11 +1,20 @@
-import React from "react";
-import { render, cleanup } from "react-testing-library";
+import React from 'react';
+import { render, cleanup } from 'react-testing-library';
 
 import 'jest-dom/extend-expect'
 
+// Ensure console error fails tests by replacing with a function that throws
+const { error: originalError } = console;
+const throwConsoleErrors = (...rest) => {
+	originalError(...rest);
+	const error = util.format.apply(this, rest);
+	throw new Error(error);
+};
+console.error = throwConsoleErrors;
+
 const ComponentWillThrow = ({ shouldThrow }) => {
 	if (shouldThrow) {
-		throw new Error("Component did throw after all");
+		throw new Error('Component did throw after all');
 	}
 
 	return <p>I guess no throw this time</p>;
@@ -13,18 +22,18 @@ const ComponentWillThrow = ({ shouldThrow }) => {
 
 afterEach(cleanup);
 
-test("renders without erroring", () => {
+test('renders without erroring', () => {
 	const { getByText } = render(<ComponentWillThrow />);
 
-	expect(getByText("I guess no throw this time")).toBeInTheDocument();
+	expect(getByText('I guess no throw this time')).toBeInTheDocument();
 });
 
-test("expect this to throw", () => {
+test('expect this to throw', () => {
 	expect(() => render(<ComponentWillThrow shouldThrow />)).toThrow();
 });
 
-test("still render without erroring", () => {
+test('still render without erroring', () => {
 	const { getByText } = render(<ComponentWillThrow />);
 
-	expect(getByText("I guess no throw this time")).toBeInTheDocument();
+	expect(getByText('I guess no throw this time')).toBeInTheDocument();
 });
